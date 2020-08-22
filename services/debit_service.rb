4e8c76257dc7c2cbaf4cc480d0805c21ed4b2bc6
Vasyl Kuzmyk <theblackarrovv@gmail.com>
2020-08-22 17:37:00 +0300

    Add DebitService

---
+++ services/debit_service.rb
@@ -0,0 +1,14 @@
+class DebitService
+  attr_reader :count, :currency, :chat_id
+
+  def initialize(count, currency, chat_id)
+    @count = count
+    @currency = currency
+    @chat_id = chat_id
+  end
+
+  def call
+    ::DebitEntity.new({count: count, currency: currency}).save
+    # recalculate_balance
+  end
+end
